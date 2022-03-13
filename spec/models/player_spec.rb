96c4c0431e75d3bb7cfbd036c3e50a48177c2897
Chris <hereloss@googlemail.com>
2022-03-13 19:12:31 +0000

    Unit tests for player added to re-calculate own global ranking

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Player, type: :model do
   before(:each) do
     @player = Player.new(first_name: 'Jonny', last_name: 'Jones', dob: '23-09-1987', nationality: 'British',
-                     rating: '1500', matchesplayed: '4', rank: 'Novice', globalranking: '1')
+                     rating: '1500', matchesplayed: '4', rank: 'Bronze', globalranking: '1')
   end
   context 'Format own data' do
     it 'will update the values to default start values upon being registered' do
@@ -11,6 +11,30 @@ RSpec.describe Player, type: :model do
       expect(@player.rating).to eq(1200)
       expect(@player.rank).to eq('Unranked')
       expect(@player.matchesplayed).to eq(0)
+    end
+
+    it 'will format its own data and return it in an array in the correct format' do
+      output = @player.player_format
+      expect(output['age']).to eq(34)
+      expect(output['full name']).to eq('Jonny Jones')
+      expect(output['current position']).to eq(1)
+      expect(output['nationality']).to eq('British')
+      expect(output['rank name']).to eq('Bronze')
+      expect(output['points']).to eq(1500)
+    end
+  end
+
+  context 'Calculate Global Ranking' do
+    it 'will recalculate its global rank and return i. j and its current rating' do
+      @user.update("globalranking": '2')
+      @user.change_user_global_ranking(0, 0, 0)
+      expect(@user.globalranking).to eq('1')
+      @user.change_user_global_ranking(1, 0, 1600)
+      expect(@user.globalranking).to eq('2')
+      @user.change_user_global_ranking(1, 1, 1600)
+      expect(@user.globalranking).to eq('3')
+      @user.change_user_global_ranking(1, 1, 1500)
+      expect(@user.globalranking).to eq('1')
     end
   end
 end
