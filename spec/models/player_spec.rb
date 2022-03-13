47707e81ceb6a2c4a5d5857ecd97b10b7db5b8d2
Chris <hereloss@googlemail.com>
2022-03-13 18:26:10 +0000

    Test added for initial values for Player object

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @player = Player.new(first_name: 'Jonny', last_name: 'Jones', dob: '23-09-1987', nationality: 'British',
+                     rating: '1500', matchesplayed: '4', rank: 'Novice', globalranking: '1')
+  end
+  context 'Format own data' do
+    it 'will update the values to default start values upon being registered' do
+      @player.format_json_updates
+      expect(@player.rating).to eq(1200)
+      expect(@player.rank).to eq('Unranked')
+      expect(@player.matchesplayed).to eq(0)
+    end
+  end
 end
