0c50ca60cb1a2a2235eb9c35c297f27b9f2c940c
Chris <hereloss@googlemail.com>
2022-03-13 18:04:59 +0000

    Test now passes for incomplete JSON

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,12 +8,30 @@ class PlayersController < ApplicationController
              status: :unprocessable_entity
       return false
     end
+    if incomplete_information == true
+      reason = 'Missing or incorrect sign up information;'
+      render json: { "created": 'fail', "reason": reason }, status: :unprocessable_entity
+      return false
+    end
 
     @player = Player.new(player_params)
     if @player.save
       render json: @player, status: :created, location: @player
     else
       render json: @player.errors, status: :unprocessable_entity
+    end
+  end
+
+  def incomplete_information
+    if params[:player_details]['first_name'].blank? || params[:player_details]['last_name'].blank? ||
+       params[:player_details]['nationality'].blank? || params[:player_details]['dob'].blank?
+      return true
+    end
+
+    parseable = begin
+      Date.strptime(params[:player_details]['dob'], '%d-%m-%Y')
+    rescue StandardError
+      true
     end
   end
 
