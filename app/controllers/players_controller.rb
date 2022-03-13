dc5d9fb28746c6ec154b840d9cf85e8b5b62d4b1
Chris <hereloss@googlemail.com>
2022-03-13 17:57:09 +0000

    Test passing

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,18 @@
 class PlayersController < ApplicationController
   def index
   end
+
+  def create
+    if params[:user_details].nil?
+      render json: { "created": 'fail', "reason": 'Blank JSON sent' },
+             status: :unprocessable_entity
+      return false
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user_details).permit(:first_name, :last_name, :dob, :nationality)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'matches/index'
   get 'players/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post 'players/create', to: 'players#create'
+  resources :players, only: %i[index show create]
+  resources :matches
 end
