88110c9a64f45bf895941aa4c10826cfa709a5f9
Chris <hereloss@googlemail.com>
2022-03-13 20:06:27 +0000

    Test added in Match controller  for updating scores

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -20,5 +20,17 @@ RSpec.describe MatchesController, type: :controller do
            params: { match_details: { winner_id: '1', winner_name: 'bill j', loser_id: '2', loser_name: 'pete g' } }
       expect(response).to have_http_status(422)
     end
+
+    it 'will record match and update scores if both players registered' do
+      Player.create(first_name: 'John', last_name: 'Jones', dob: '23-09-1987', nationality: 'Scottish',
+                            rating: '800', matchesplayed: '0', rank: 'Unranked', globalranking: '1')
+      Player.create(first_name: 'Joan', last_name: 'Johnson', dob: '23-09-1987', nationality: 'Scottish',
+                           rating: '800', matchesplayed: '0', rank: 'Unranked', globalranking: '1')
+      post :create,
+           params: { match_details: { winner_id: '1', winner_name: 'John Jones', loser_id: '2',
+                                      loser_name: 'Joan Johnson' } }
+      expect(response).to have_http_status(201)
+      expect(response.body).to include('880', '720')
+    end
   end
 end
