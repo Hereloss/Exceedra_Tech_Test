ee784ea4b6632b03bd3dfc4fed6dd0509c613dbb
Chris <hereloss@googlemail.com>
2022-03-13 19:55:19 +0000

    First match test passing

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,10 @@
 class MatchesController < ApplicationController
-  def index
+
+  def create
+    if params[:match_details].blank? || params[:match_details]['winner_name'].blank? || params[:match_details]['loser_name'].blank?
+      render json: { "created": 'fail', "reason": 'Blank or Incomplete JSON sent' },
+             status: :unprocessable_entity
+      return false
+    end
   end
 end
--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Matches", type: :request do
-  
+  describe "POST " do
+    it "returns http failure" do
+      post "/matches"
+      expect(response).to have_http_status(422)
+    end
+  end
 end
