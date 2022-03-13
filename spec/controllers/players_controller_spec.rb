e0d1c203cf9bcaff1f501483beb5f8c2261765b9
Chris <hereloss@googlemail.com>
2022-03-13 17:53:19 +0000

    Players POST request test added

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe PlayersController, type: :controller do
 
   end
 
+  describe 'POST /' do
+    it 'should respond to a post request and will return error if no JSON given' do
+      post :create
+      expect(response).to have_http_status(422)
+    end
+
+  end
+
 end
